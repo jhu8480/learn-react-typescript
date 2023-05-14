cda469b1779f19c24b7eeca3f7aea61236741927
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-05-13 20:51:40 -0400

    course 15% complete

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,6 +1,20 @@
-
 function ListGroup() {
-  return <h1>List Group</h1>
+  let items = ["New York", "Shanghai", "Tokyo", "London", "Paris"];
+  items = [];
+
+  return (
+    <>
+      <h1>City List</h1>
+      {items.length === 0 && <p>No items found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li className="list-group-item" key={index}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
-export default ListGroup
+export default ListGroup;
