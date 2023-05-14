0a5b4eedbebaf719e9c41021845a29c37aa2f40f
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-05-14 19:00:59 -0400

    19% complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  
+  const items = ["New York", "Shanghai", "Tokyo", "London", "Paris"];
+  const onSelectItem = (item: string) => console.log(item);
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={items} heading={"Cities"} onSelectItem={onSelectItem} />
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,32 @@
-function ListGroup() {
-  let items = ["New York", "Shanghai", "Tokyo", "London", "Paris"];
-  items = [];
+import { useState } from "react";
+
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
+  const [selectedIndex, handleSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>City List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={index}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={index}
+            onClick={() => {
+              handleSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
             {item}
           </li>
         ))}
